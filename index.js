14ff9b8dcccf046b87e6a1f79cf3b21fef554bd4
Lee Kopacz <lee_kopacz@yahoo.com>
2022-08-31 12:14:27 -0500

    commit tree 3ca0bce516f69d4dbeabb0b32edc9582e7bd1d85

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+
+const employee = {
+    name: "Lee",
+    streetAddress: "5811 Sherman Ave",
+}
+ 
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee }
+
+    newEmployee[key] = value
+    return newEmployee
+}
+ 
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value 
+    return obj
+}
+
+function deleteFromEmployeeByKey (newEmployee, key, value) {
+    newEmployee [key] = value
+   delete newEmployee.key
+    return employee.name = "Sam"
+    
+}
+
+function destructivelyDeleteFromEmployeeByKey (obj, key, value) {
+    obj [key] = value
+return delete obj.key
+}
+
+
